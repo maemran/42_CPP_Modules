7c2429d60b5bfd37ee584e0622ea2b580bd6e91c
maemran <maemran@student.42amman.com>
2025-09-06 09:49:54 +0300

    working on ex04

---
+++ CPP01/ex04/main.cpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maemran < maemran@student.42amman.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/04 20:29:01 by maemran           #+#    #+#             */
+/*   Updated: 2025/09/06 07:22:28 by maemran          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fstream>
+#include <string>
+#include <iostream>
+/// read it line by line 
+// take the line once change the word in the line move it into .replace file
+
+void    replace_word(char *old_word, char *new_word, std::string& line)
+{
+    std::string oldStr = old_word;
+    std::string newStr = new_word;
+
+    size_t pos = line.find(oldStr);
+    if (pos != std::string::npos) 
+    {
+        line.erase(pos, oldStr.length());
+        line.insert(pos, newStr);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    std::ifstream   file(argv[1]);
+    std::ofstream   outFile("outfile.replace");
+    std::string     read_line;
+    int i = 0;
+    
+    if (argc != 4)
+        return (1);
+    if (!file.is_open() || !outFile.is_open())
+        std::cerr << "Failed to open file." << std::endl;
+    while (1)
+    {
+        if (i != 0)
+            outFile << "\n";
+        std::getline(file, read_line);
+        replace_word(argv[2], argv[3], read_line);
+        outFile << read_line;
+        if (file.eof())
+            break;
+        i++;
+    }
+}
